785bb7f3b3d133fb14db5a809b04cac83978893e
Haunted Loaf <haunty@bread.social>
2023-06-17 17:15:49 +0100

    Make character stand "up" inside tube

--- addons/character_controller/character_controller.gd
+++ addons/character_controller/character_controller.gd
@@ -12,7 +12,21 @@ var double_tap_timeout = 1.0
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func rotation_to(from: Vector3, to: Vector3) -> Quaternion:
+  var cross = from.cross(to)
+  var dot = from.dot(to)
+  var theta = atan2(cross.length(), dot)
+  return Quaternion(cross.normalized(), theta)
+
 func _physics_process(delta):
+  var new_up = (global_position * -Vector3(1, 1, 0)).normalized()
+  var cur_up = global_transform.basis.y.normalized()
+  var rot = rotation_to(cur_up, new_up)
+#  print(rot.get_angle())
+  global_transform.basis = global_transform.basis.rotated(rot.get_axis().normalized(), rot.get_angle())
+  velocity = velocity.rotated(rot.get_axis().normalized(), rot.get_angle())
+  
+#  look_at(global_position + global_transform.basis.z, (global_position * -Vector3(1, 1, 0)).normalized())
   double_tap_timer = max(0.0, double_tap_timer - delta)
   var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
   var up_down = Input.get_axis("move_down", "move_up")
@@ -21,7 +35,7 @@ func _physics_process(delta):
     position.y += sin(Time.get_ticks_msec() / 1000.0) / 1000.0
     if Input.is_action_just_pressed("move_down") or Input.is_action_just_pressed("move_up"):
       if double_tap_timer > 0.0:
-        flying = false
+#        flying = false
         double_tap_timer = 0.0
       else:
         double_tap_timer = double_tap_timeout
